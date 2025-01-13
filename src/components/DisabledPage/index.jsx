5992a4f6af951cd658368dad044a22240bfb92ab
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-01-14 03:27:15 +0500

    Disabled Page added

--- src/components/DisabledPage/index.jsx
+++ src/components/DisabledPage/index.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { DisabledPageContainer } from "./style";
 import { TopTitle } from "../../pages/login/style";
+import { useLanguage } from "../../context/LanguageContext";
 
 const DisabledPage = () => {
+  const { translate } = useLanguage();
+
   return (
     <DisabledPageContainer>
-      <TopTitle>Sahifa API ulanishini kutmoqda...</TopTitle>
+      <TopTitle>{translate("waiting_API")}</TopTitle>
     </DisabledPageContainer>
   );
 };
