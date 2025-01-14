84eb559f31d93b33630e0a56a8bc42f33d0d3509
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-01-15 02:31:32 +0500

    123

--- src/utils/server/server.js
+++ src/utils/server/server.js
@@ -321,7 +321,7 @@ const fetchDistrict = async (districtId) => {
       `/v1/auth/district?districtId=${districtId}`
     );
     console.log(response?.data);
-    return response?.data; // NOTE region infor (uz ru eng crylic)
+    return response?.data; // Region nomini qaytaradi
   } catch (error) {
     console.error("Error fetching region data", error);
     return null;
