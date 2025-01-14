003ce24bddd3a15195e91deb4905b15fa30e1054
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-01-15 02:23:14 +0500

    API changed district and region

--- src/pages/SettingsMenager/Table.jsx
+++ src/pages/SettingsMenager/Table.jsx
@@ -9,6 +9,9 @@ import RightArrow from "../../assets/svg/RightArrow";
 import styled from "styled-components";
 import ModalManager from "./Modal";
 import { useLanguage } from "../../context/LanguageContext";
+import { useGetDistrictById } from "../../utils/server/server"; // Import to'g'ri qilingan
+import Instance from "../../utils/Instance";
+import { DatFormatter } from "../../utils/DatFormatter";
 
 const Container = styled.div`
   position: relative;
@@ -22,7 +25,6 @@ const Table = ({ title = "", data = [], isLoading = false }) => {
 
   const itemsPerPage = 10;
   const totalPages = Math.ceil(data.length / itemsPerPage);
-  const { mutate, data: ResponseData, isError } = useGetDistrictById();
 
   const handleNext = () => {
     if (currentPage < totalPages - 1) {
