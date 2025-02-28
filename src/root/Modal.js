c22ae97d61d57dbeee298aeb23c4dfb228a3cbca
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-02-28 17:48:51 +0500

    Reports Ready

--- src/root/Modal.js
+++ src/root/Modal.js
@@ -139,3 +139,32 @@ export const DeleteBtn = styled.div`
         font-size: 12px;
     }
 `;
+
+export const ContainerInner = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+
+    border-radius: ${({ brdr }) => (brdr ? brdr : "57px")};
+    background-color: #f7f8fc;
+
+    position: relative;
+
+    padding-right: ${({ rightIcon }) => (rightIcon ? "50px" : "0")};
+    > .rightIcon {
+        padding: 10px;
+
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        cursor: pointer;
+    }
+
+    > .input {
+        padding: 20px;
+        width: 100%;
+        height: 100%;
+    }
+`
