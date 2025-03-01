3a32aee7b8d2bc2dd0353ee3a3f4615f7e0539b5
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-03-02 04:44:44 +0500

    ADMiN: Workplace bug fixed

--- src/rolls/admin/MestaRabota/index.jsx
+++ src/rolls/admin/MestaRabota/index.jsx
@@ -36,7 +36,7 @@ function AdminMestaRabota() {
 
 
   const { data: workplaces, isLoading: isloadingWorkplaces } =
-    useGetWorkplacesDb();
+    useGetWorkplacesDb({});
   const queryClient = useQueryClient();
 
 
